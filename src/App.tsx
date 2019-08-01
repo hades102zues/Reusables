bc5f6bf8c15bd7e314bf1128842cf0a6e96d5f78
Joshua Wiggins <joshua_kar@hotmail.com>
2019-08-01 11:02:55 -0400

    Adds header. Adds header connected to side drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import styles from './App.module.css';
 
 import SideDrawer from './SideDrawer/SideDrawer';
-
-
+import Header from './Header/Header';
 
 
 interface State {
   displayBool : boolean
 }
+
 class App extends React.Component<{}, State> {
 
     constructor (props:{}) {
@@ -21,26 +21,26 @@ class App extends React.Component<{}, State> {
 
     //changes the boolean that controls the open state of the sidedrawer
     clickHandler = () =>{
-      
       this.setState( {displayBool : !this.state.displayBool} );
     }
 
     render () {
       return (
         <React.Fragment>
-            
-              <SideDrawer 
+
+            <Header drawerHandler={this.clickHandler} />
+           <SideDrawer 
               display={this.state.displayBool} 
               stateHandler = {this.clickHandler}
-              
-              />
-              <button onClick={this.clickHandler}>click me</button>
+            />
+
+             
              
         </React.Fragment>
 
       );
     }
  
-}
+  }
 
 export default App;
---
+++ src/Header/Header.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './Header.module.css';
+import Hamburger from './Hamburger/Hamburger';
+
+interface Props {
+    drawerHandler: ()=> any
+}
+
+const header:React.FC<Props> = (props) => (
+
+    <header className={styles.header}>
+    <div className={styles.header__wrapper}>
+        <div className={styles.logoSpace}>
+             <div className={styles.logo}>
+              <img alt="company_logo" className={styles.logo__image} />
+              <h3 className={styles.logo__company}>Company Name</h3>
+             </div>
+       </div>
+
+      <div className={styles.navSpace}>
+            <nav className={styles.nav}>
+                  <ul className={styles.nav__list}>
+                        <li className={styles.nav__link}>JOshua</li>
+                        <li className={styles.nav__link}>JOshua</li>
+                        <li className={styles.nav__link}>JOshua</li>
+                        <li className={styles.nav__link}>JOshua</li>
+                        <li className={styles.nav__link}>JOshua</li>
+                  </ul>
+                   <Hamburger click={props.drawerHandler} />
+            </nav>
+      </div>
+      
+  </div>
+  </header>
+);
+
+ export default header;
